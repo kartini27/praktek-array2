602e3c1629ba32a94d3db3ffc23832ffbe52894f
kartini <126692640+kartini27@users.noreply.github.com>
2023-03-07 07:30:17 -0800

    Add files via upload

---
+++ coba-array1.js
@@ -0,0 +1,39 @@
+let wadah1 = 'merica';
+let wadah2 = 'minyak';
+let wadah3 = 'gula';
+let wadah4 = 'garam';
+let wadah5 = 'saos';
+let wada6 = 'lada';
+let wadah = ['merica','minyak','gula','garam','saos','lada'];
+console.log(wadah[0]);
+console.log(wadah[wadah,length - 1]);
+console.log(wadah,length)
+console.log('==========');
+console.log('push array');
+wadah.push('kecap','cabe')
+console.log(wadah,length)
+console.log(wadah)
+console.log('============');
+console.log('ganti data array');
+wadah[0] = 'jahe'
+wadah[3] = 'air'
+console.log (wadah)
+console.log(wadah,length)
+console('============');
+console.log('push dari depan');
+wadah.unshift('mangga')
+console.log(wadah)
+
+// index genap 
+console.log(wadah[2]);
+console.log(wadah[4]);
+console.log(wadah[6]);
+console.log(wadah[8]);
+console.log(wadah[10]);
+console.log(wadah[12]);
+console.log(wadah[14]);
+console.log(wadah[16]);
+console.log(wadah[18]);
+console.log(wadah[20]);
+console.log(wadah[22]);
+console.log(wadah[24]);
